6b60baa5c7d0a365091c281e44631807d77c8ec6
Anuj Maurya <anuj24122001@gmail.com>
2025-04-23 17:44:07 +0530

    server ready

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+import { apiRouter } from './routes/apiRoutes.js'
+
+const PORT =8000
+
+const app = express()
+
+
+app.use('/api', apiRouter)
+app.use((req, res) => {
+    res.status(404).json({ message: "Endpoint not found. Please check the API documentation." })
+})
+
+app.listen(PORT, ()=>console.log(`The server is connected on the port ${PORT}`))
